b5d36fae90eb60090d76f0f6caa70d5194df5610
Sergio Duarte <holasoysergio.805@gmail.com>
2025-02-13 22:40:10 -0500

    SyntaxFacade return Symbol in parse method

--- src/main/java/org/example/cup/SyntaxFacade.java
+++ src/main/java/org/example/cup/SyntaxFacade.java
@@ -1,17 +1,21 @@
 package org.example.cup;
 
+import java_cup.runtime.Symbol;
+
 import java.io.StringReader;
 
 public class SyntaxFacade {
+
+    public Syntax syntax;
 
     /**
      * A syntax analyzer function
      * @param code Code to be analyzed
      * @throws Exception The code doesn't comply with the rules
      */
-    public void parse(String code) throws Exception {
-        Syntax syntax = new Syntax(new DSLCup(new StringReader(code)));
-        syntax.parse();
+    public Symbol parse(String code) throws Exception {
+        syntax = new Syntax(new DSLCup(new StringReader(code)));
+        return syntax.parse();
     }
 
 }
